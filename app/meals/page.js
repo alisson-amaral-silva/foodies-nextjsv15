1f5a1bc78e6c809b8f0c9a8a0110408cc3b5b96f
Alisson Amaral <aalissonamaral98@gmail.com>
2025-02-07 16:10:36 -0300

    add fetch data

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from 'next/link'
 import styles from './page.module.css'
 import MealsGrid from '@/components/Meals/MealsGrid/meals-grid'
+import { getMeals } from '@/lib/meals'
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals()
+  
   return (
     <>
       <header className={styles.header}>
@@ -16,7 +19,7 @@ export default function MealsPage() {
         </p>
       </header>
       <main>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   )
