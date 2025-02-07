63a96428c01d6a11a3549474415f0c2155147b8e
Alisson Amaral <aalissonamaral98@gmail.com>
2025-02-07 15:25:07 -0300

    add content into community and fix header links

--- components/MainHeader/main-header.js
+++ components/MainHeader/main-header.js
@@ -3,27 +3,28 @@ import logoImg from '@/assets/logo.png'
 import Link from 'next/link'
 import styles from './main-header.module.css'
 import MainHeaderBackground from '../MainHeaderBackground/main-header-background'
+import NavLink from '../NavLink/nav-link'
 
 export default function MainHeader() {
   return (
     <>
-    <header className={styles.header}>
-      <Link className={styles.logo} href="/">
-        <Image src={logoImg} alt="logo image" priority />
-        Next Level Food
-      </Link>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodie  Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={logoImg} alt="logo image" priority />
+          Next Level Food
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals">Browse Meals</NavLink>
+            </li>
+            <li>
+              <NavLink href="/community">Foodie Community</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <MainHeaderBackground />
     </>
   )
 }
