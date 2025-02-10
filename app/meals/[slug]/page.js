3ff0574a0caab1f54dd77ad65e2d98e67e4173f5
Alisson Amaral <aalissonamaral98@gmail.com>
2025-02-10 12:39:57 -0300

    add meal by id and share form

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,3 +1,39 @@
+import Image from 'next/image'
+import style from './page.module.css'
+import { getMealBySlug } from '@/lib/meals'
+import { notFound } from 'next/navigation'
+
 export default function MealPage({ params }) {
-  return <h1>Meal {params.slug}</h1>
+  const meal = getMealBySlug(params.slug)
+
+  if (!meal) {
+    notFound()
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />')
+
+  return (
+    <>
+      <header className={style.header}>
+        <div className={style.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={style.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={style.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={style.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={style.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  )
 }
