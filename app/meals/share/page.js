c8bf20c6309359a6727631b592d5253dd39166c4
Alisson Amaral <aalissonamaral98@gmail.com>
2025-02-10 18:06:06 -0300

    add store data

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,9 @@
 import ImagePicker from '@/components/ImagePicker/image-picker'
 import styles from './page.module.css'
+import { handleShareMealFormSubmit } from '@/lib/actions'
 
 export default function ShareMealPage() {
+
   return (
     <>
       <header className={styles.header}>
@@ -11,7 +13,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form}>
+        <form className={styles.form} action={handleShareMealFormSubmit}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +41,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker name="image" />
+          <ImagePicker label="Your image" name="image" />
           <p className={styles.actions}>
             <button type="submit">Share Meal</button>
           </p>
